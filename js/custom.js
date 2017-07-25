5d94fe3f4791a0095e216963331c9e75ca5bbbe9
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-07-24 22:27:06 -0500

    Fixes for navbar

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
     'use script';
 
-    if ($(window).scrollTop()<100){
+    if ($(window).scrollTop()<80){
 
       $('.navbar').css({
 
@@ -27,7 +27,7 @@ $(document).ready(function(){
         'opacity':'1'
       });
       $('.navbar-default').css({
-
+        'visibility':'visible',
         'background-color':'rgba(21,46,71,0.9)',
         'border-color':'#132c44'
       });
