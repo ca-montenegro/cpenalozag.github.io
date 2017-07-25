1a24a20d4ae36ccb3c25779a82d1f1679e5336ab
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-07-24 22:56:36 -0500

    Footer

--- js/custom.js
+++ js/custom.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
     'use strict';
 
 
-    $('.nav-item, .contact-down, .about-down').click(function() {
+    $('.nav-item, .contact-down, .about-down, #to-top').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
