333b608a2984b4bbe64b28d48e250cc66c79e979
CarlosP97 <c.penaloza@uniandes.edu.co>
2017-07-24 22:05:37 -0500

    Navbar initial

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+/* Show and hide navbar */
+$(document).ready(function(){
+
+  'use strict';
+
+  $(window).scroll(function(){
+
+    'use script';
+
+    if ($(window).scrollTop()<100){
+
+      $('.navbar').css({
+
+        'margin-top':'-100px',
+        'opacity':'0'
+
+      });
+      $('.navbar-default').css({
+
+        'background-color':'rgba(21,46,71,0)'
+      });
+    }
+    else{
+
+      $('.navbar').css({
+        'margin-top':'0px',
+        'opacity':'1'
+      });
+      $('.navbar-default').css({
+
+        'background-color':'rgba(21,46,71,0.9)',
+        'border-color':'#132c44'
+      });
+
+    }
+
+  });
+
+});
